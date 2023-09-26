b034f602b33b0c0026c4235b66bb852c5a6c26fe
Chhavi Choudhary <chhavi.2023@tecorb.co>
2023-09-26 15:09:39 +0530

    first commit

---
+++ test/videos/app/sideBar/page.tsx
@@ -0,0 +1,14 @@
+
+
+export default function SideBar(){
+
+    return(
+        <>
+        <ul>
+            <li>Lesson 1</li>
+            <li>Lesson 2</li>
+            <li>Lesson 3</li>
+        </ul>
+        </>
+    )
+}
