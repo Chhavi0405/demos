f2b39487a4b6b384a2a3af027eada20d576840f0
Chhavi Choudhary <chhavi.2023@tecorb.co>
2023-09-28 19:10:47 +0530

    video play ,duration

--- test/videoapp/app/sidebar/page.tsx
+++ test/videoapp/app/sidebar/page.tsx
@@ -1,16 +1,16 @@
-
-import { useRouter } from "next/router";
+ 'use client'
+import { useRouter } from "next/navigation";
 export default function Sidebar () {
 const router = useRouter();
     const dataArray = [
-        { id: "vid1", videosrc: "Topic 1" ,videoTitle:"q"},
-        { id: "vid2", videosrc: "Topic 2" ,videoTitle:"w"},
-        { id: "vid3", videosrc: "Topic 3" ,videoTitle:"e"},
-        { id: "vid4", videosrc: "Topic 4" ,videoTitle:"r"},
-        { id: "vid5", videosrc: "Topic 5" ,videoTitle:"t"},
-        { id: "vid6", videosrc: "Topic 6" ,videoTitle:"y"},
+        { id: "vid1", videosrc: "Topic 1" ,videoTitle:"q1547"},
+        { id: "vid2", videosrc: "Topic 2" ,videoTitle:"aircraftw"},
+        { id: "vid3", videosrc: "Topic 3" ,videoTitle:"lines"},
+        { id: "vid4", videosrc: "Topic 4" ,videoTitle:"monrach"},
+        { id: "vid5", videosrc: "Topic 5" ,videoTitle:"reading"},
+        { id: "vid6", videosrc: "Topic 6" ,videoTitle:"versus"},
       ];
-      console.log(dataArray,"array")
+    //   console.log(dataArray,"array")
 
       
   const handleClick = (t: any) => {
@@ -18,24 +18,20 @@ const router = useRouter();
 
   };
     return (
-        <>
-        <div style={{textAlign:"left"}}>
+      <>
+       <div style={{textAlign:"left"}}>
             <h1 >List</h1>
             
         {dataArray.map((data:any)=>{return(
             <div key={data.id} 
             style={{color:"red"}}
-            onClick={(t:any)=>{handleClick(`${data.id}`)}}>
+            onClick={()=>{handleClick(`${data.id}`)}}>
                 {data.videoTitle}
             </div>
         )}
         )}
         </div>
 
-        <div>
-        <video />
-
-        </div>
-        </>
-    )
+      </>
+    );
 }
