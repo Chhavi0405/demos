cb0eefbaa83d5450d55529e0e1551e16efb75975
Chhavi Choudhary <chhavi.2023@tecorb.co>
2023-10-20 17:36:38 +0530

    custom

---
+++ custom-hook/app/component/page.tsx
@@ -0,0 +1,34 @@
+import { daysToWeeks, formatDistance } from "date-fns"
+
+
+   export const days2Week=(value:number)=>{
+        return(daysToWeeks(value))
+     }
+
+     export const formatDistanceDate =(date1:Date|number ,date2:Date|number)=>{
+              return  (formatDistance(date1,date2))
+             }
+
+
+// 'use client'
+// import { daysToWeeks, formatDistance, isAfter } from "date-fns";
+// import { useState } from "react"
+
+// export default function Component(){
+//     const [isInput,setIsInput] = useState<string | number |Date | boolean>("")
+//     // const [isFormat,setIsFormat] = useState()
+
+//     const values = new Date();
+//     console.log(values,"value")
+ 
+//      function days2Week(value:number){
+//         setIsInput(daysToWeeks(value))
+//      }
+     
+//      function isAfterdate(value:Date|number,compvalue:Date|number){
+//         setIsInput(isAfter(value,compvalue))
+//      }
+
+//     
+
+// }
